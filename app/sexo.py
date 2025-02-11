3d943d0670d0651b3d200bfdd65bcbb4c8900760
Didi <ddgrisalesg@uqvirtual.edu.co>
2025-02-10 23:28:34 -0500

    task full sexo

--- app/sexo.py
+++ app/sexo.py
@@ -1,27 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# URL del archivo CSV github
+# URL del archivo CSV
 archivo_csv = "https://raw.githubusercontent.com/DidiGG/Graficas-Pacientes-HomeMed/main/data/lista_pacientes.csv"
 
-# Cargar el dataset (csv)
+# Cargar el dataset
 df = pd.read_csv(archivo_csv)
 print(df.columns)
 columna_sexo = "sexo"  
 
-# Cantidad de pacientes por sexo
+# Contar la cantidad de pacientes por sexo
 conteo_sexos = df[columna_sexo].value_counts().sort_index()
 conteo_sexos.index = conteo_sexos.index.map({"M": "Masculino", "F": "Femenino"})
 
-# Mapeo de colores para cada sexo
-color_mapping = {"Masculino": "blue", "Femenino": "pink"}
-colors = [color_mapping[sexo] for sexo in conteo_sexos.index]
-
-# Gráfico de barras
+# Crear el gráfico
 plt.figure(figsize=(15, 7))
-bars = plt.bar(conteo_sexos.index, conteo_sexos.values, color=colors)  # Apply color mapping
+bars = plt.bar(conteo_sexos.index, conteo_sexos.values, color=["pink", "blue"])
 
-# Valores exactos sobre cada barra
+# Añadir los valores exactos sobre cada barra
 for bar in bars:
     yval = bar.get_height()
     plt.text(bar.get_x() + bar.get_width()/2, yval + 1, int(yval), ha='center', va='bottom')
